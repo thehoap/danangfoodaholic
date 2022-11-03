361979e07a56a24b51edf2aa7cf2483b269decaa
thehoap <thehoap.2000@gmail.com>
2022-11-03 23:56:32 +0700

    init base api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { errorHandler } from './middleware/errorMiddleware.js';
 import baseRouter from './routes/apiRoutes.js';
@@ -11,9 +12,16 @@ const port = process.env.PORT;
 connectDB();
 
 const app = express();
+const corsOption = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    exposedHeaders: ['x-auth-token'],
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOption));
 
 app.use('/api', baseRouter);
 
