ba2efb2691ba03c945aed272499956831416cedd
thehoap <thehoap.2000@gmail.com>
2022-12-04 12:03:12 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { errorHandler } from './middleware/errorMiddleware.js';
 import baseRouter from './routes/apiRoutes.js';
@@ -26,6 +28,19 @@ app.use(cors(corsOption));
 app.use('/api', baseRouter);
 
 app.use(errorHandler);
+
+// Serve Frontend
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, () =>
     console.log(`Server started on port http://localhost:${port}`)
