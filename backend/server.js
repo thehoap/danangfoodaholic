3e99e8c0f2b0d7f63a465283ab96bb5301dfffe9
thehoap <thehoap.2000@gmail.com>
2022-12-04 14:00:32 +0700

    fix server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { errorHandler } from './middleware/errorMiddleware.js';
 import baseRouter from './routes/apiRoutes.js';
@@ -31,14 +33,14 @@ app.use(errorHandler);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.get('*', (req, res) =>
-//         res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-//     );
-// } else {
-//     app.get('/', (req, res) => res.send('Please set to production'));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, () =>
     console.log(`Server started on port http://localhost:${port}`)
